77a168c58cff143b4c75b06e86334d3d4f353787
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-02-06 15:35:52 +0100

    Apache Log4j 2.12.3

--- 2.x/log4j-docker/apidocs/package-search-index.js
+++ 2.x/log4j-docker/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.logging.log4j.docker"},{"l":"org.apache.logging.log4j.docker.model"}]
+packageSearchIndex = [{"l":"org.apache.logging.log4j.docker"},{"l":"org.apache.logging.log4j.docker.model"}]
--- 2.x/log4j-spring-cloud-config/log4j-spring-cloud-config-client/apidocs/package-search-index.js
+++ 2.x/log4j-spring-cloud-config/log4j-spring-cloud-config-client/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.logging.log4j.spring.cloud.config.client"}]
+packageSearchIndex = [{"l":"org.apache.logging.log4j.spring.cloud.config.client"}]
